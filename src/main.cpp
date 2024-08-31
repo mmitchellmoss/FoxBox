52d0fa283ed27573901b1cbfecfa46c91f08e6eb
Mitch Moss <mmitchellmoss@gmail.com>
2024-08-31 13:30:19 -0500

    Do initial code updates for project

--- src/main.cpp
+++ src/main.cpp
@@ -1,211 +1,284 @@
-/*Arduino controlled Fox Hunting Xmtr and / or Beacon 
-Code cobbled together from various sources
+/* 
+Arduino controlled Fox Hunting Xmtr and/or Beacon.
+Code cobbled together from various sources.
 http://ttg.org.au/arduino_fox.php
 http://www.wt4y.com/arduino-fox-controller
 http://www.wcar.ca/photo-gallery/fox-tx/
-Pin A0 is the input for DMTF, see pic for pin out
-Pin 5 is the square wave output for the Morse Code, see pic for pin out
-Pin 7 is Push to Talk pin (low = PTT, Signal to relay
-Useing your DMTF 
+
+Pin A0 is the input for DTMF, see pic for pin out.
+Pin 5 is the square wave output for the Morse Code, see pic for pin out.
+Pin 7 is Push to Talk pin (low = engage relay for PTT).
+
+Using your DTMF 
 1 = Start
 5 = hi in morse code
-6 = Call sing you entered
+6 = Call sign you entered
 7 = 73
+Any other key = Stop
 */
 
+
+/* To Do
+*  Make time between beacons optionally random.
+*  Change time between beacons updatable programatically via DTMF.
+*  Add a repeat count parameter to sendMessage to automatically repeat the message x number of times.
+*  Maybe switch to dash duration, end of word duration, etc.
+*/
+
+
 #include <Arduino.h>
+// #include <Tone32.h>   // Only needed for ESP32.
 #include <DTMF.h>
 #include "pitches.h"
+#include "Timer.h"
 
-int sensorPin = A0;  //The following block used by DTMF
-float n = 128.0;
-float sampling_rate = 8926.0;
-DTMF dtmf = DTMF(n, sampling_rate);
-float d_mags[8];
-char thischar;
 
-int xmit = 0;  // if xmit = 0, don't transmit
-int pause = 1440;  // pause between transmissions, 22 = 1 second (approx)
+// Function declarations.
+void playMelody();
+void sendMessage(const char* msg);
+void sendCharacter(const char c);
+void sendMorseCode(const String tokens);
+void sendEndOfWord();
+void sendDot();
+void sendDash();
+void morseToneWait(const int n);
+void enablePTT();
+void disablePTT();
 
-// notes in the melody:
-int melody[] = {
-  NOTE_G4, NOTE_E4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_G4, NOTE_G4, NOTE_E4
-};
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = {
-  8, 8, 4, 4, 8, 8, 8, 8, 4, 4, 4, 4
-};
 
-#define PTT  7
-#define BUZZER_PIN 5  // send music and cw out this pin
-#define BUZZER_FREQUENCY 700  // cw pitch
-char beaconstring[] = "CQ VA6WCT/FOX CQ VA6WCT/FOX"; // change to your call sign
-char idstring[] = "DE VA6WCT"; // change to your call sign
-int randNumber;
+// General configuration.
+const int   PIN_PTT         { 7 };          // Pin for Push to talk relay.
+const int   PIN_BUZZER      { 5 };          // Send music and CW out this pin.
+const int   CW_FREQ         { 700 };        // CW pitch.
+const int   ADC_BITDEPTH    { 1024 };       // Number of discrete reading the controllers ADC can distinguish. 
+const char  beaconString[]   = "KI4OOK/FOX KI4OOK/FOX"; 
+const char  idString[]       = "DE KI4OOK"; 
+enum  class State           { WAIT, BEACON };
+State  state = State::WAIT;
 
-char chartemp[4] ;
-char incomingByte = 0;
-//int morseOutput = 4; //Pin for receiving Morse output - attach LED to this pin
-int interval = 60;   //length of time for one dot - basic unit of measurement;  one interval occurs between each dot or dash; dash is equivalent to 3 dots; 7 dots between words
+// DTMF related.
+const int   PIN_DTMF        { A0 };         // Pin to read DTMF signals from.
+const float dtmfBlockSize   { 128.0 };      // DTMF block size. I'm not sure why this is a float.
+const float dtmfSampleRate  { 8926.0 };     // DTMF sample rate. I'm not sure why this is a float.
+float       d_mags[8];                      // Array for DTMF magnitude readings.
+DTMF        dtmf(dtmfBlockSize, dtmfSampleRate);
 
+// Transmission related.
+bool        beaconEnabled   { false }; 
+const int   dotDuration     { 60 };         // Length of time for one dot. Basic unit of measurement. 
+M3::Timer   timerBeacon(M3::Timer::COUNT_DOWN);
+
+// Morse code definition.
 String alphabet = "abcdefghijklmnopqrstuvwxyz1234567890/ ";
 String ALPHABET = alphabet;
 
 String morseCode[] = { 
-    ".-", // A
-    "-...", // B
-    "-.-.", // C
-    "-..", // D
-    ".", // E
-    "..-.", // F
-    "--.", // G
-    "....", // H
-    "..", // I
-    ".---", // J
-    "-.-", // K
-    ".-..", // L
-    "--", // M
-    "-.", // N
-    "---",  // O
-    ".--.", // P
-    "--.-", // Q
-    ".-.", // R
-    "...",  // S
-    "-", // T
-    "..-", // U
-    "...-", // V
-    ".--", // W
-    "-..-", // X
-    "-.--", // Y
-    "--..", // Z
-    ".----", // 1
-    "..---", // 2
-    "...--", // 3
-    "....-", // 4
-    ".....", // 5
-    "-....", // 6
-    "--...", // 7
-    "---..", // 8
-    "----.", // 9
-    "-----", // 0
-    "-..-.", // forward slash
-    " "   //space character
+    ".-",       // A
+    "-...",     // B
+    "-.-.",     // C
+    "-..",      // D
+    ".",        // E
+    "..-.",     // F
+    "--.",      // G
+    "....",     // H
+    "..",       // I
+    ".---",     // J
+    "-.-",      // K
+    ".-..",     // L
+    "--",       // M
+    "-.",       // N
+    "---",      // O
+    ".--.",     // P
+    "--.-",     // Q
+    ".-.",      // R
+    "...",      // S
+    "-",        // T
+    "..-",      // U
+    "...-",     // V
+    ".--",      // W
+    "-..-",     // X
+    "-.--",     // Y
+    "--..",     // Z
+    ".----",    // 1
+    "..---",    // 2
+    "...--",    // 3
+    "....-",    // 4
+    ".....",    // 5
+    "-....",    // 6
+    "--...",    // 7
+    "---..",    // 8
+    "----.",    // 9
+    "-----",    // 0
+    "-..-.",    // forward slash
+    " "         //space character
 };
 
+
+
+
 void setup() {
+    pinMode(PIN_PTT, OUTPUT);
+    disablePTT();
   
-  pinMode(PTT, OUTPUT);
-  digitalWrite(PTT, HIGH);
-  Serial.begin(9600);
-//  pinMode(morseOutput, OUTPUT);
-  
-  ALPHABET.toUpperCase();
-  randomSeed(analogRead(0));  // in case random delays between 
-                              // transmissions are used
+    ALPHABET.toUpperCase();
+    randomSeed(analogRead(0));  // In case random delays between transmissions are used.
 }
+
+
+
 
 void loop() {
 
-for (int x = 0; x < pause; x ++)  // while waiting between transmissions, look for DTMF
-{
-//  Serial.println(x);
-  dtmf.sample(sensorPin);
-  dtmf.detect(d_mags, 506);
-  thischar = dtmf.button(d_mags, 1800.);
-  
-  if (thischar) {                             // decide what to do if DTMF tone is received
-    switch (thischar) {
-      case 49:  // the number 1
-        xmit = 1;                            // set the flag to enable transmissions
-        break;
-      case 53:  //number 5                   // Sends morse code for "hi"
-        digitalWrite(PTT, LOW);
-        delay(2000);
-        sendLetter('h');
-        sendLetter('i');
-        digitalWrite(PTT, HIGH);
-        delay(2000);
-        break;
-      case 54:  //number 6                  // Sends morse code with callsing from line 41 
-        digitalWrite(PTT, LOW);
-        delay(2000);
-        for (int i = 0; i < sizeof(idstring); i++){
-          sendLetter(idstring[i]);
+    switch (state) {
+        case State::WAIT:
+            // Always check to see if it is time to do beacon stuff.
+            if (beaconEnabled && timerBeacon.isTimerExpired()) {
+                state = State::BEACON;
+                break;
+            }
+
+            // Check for DTMF tone and do something with it if one exists.
+            dtmf.sample(PIN_DTMF);
+            dtmf.detect(d_mags, ADC_BITDEPTH);
+            char thischar = dtmf.button(d_mags, 1800.);
+
+            if (thischar) {                             
+                switch (thischar) {
+                    case 49:  // Number 1                   // Enables beacon transmission.
+                        beaconEnabled = true;                           
+                        state = State::BEACON;
+                        break;
+                    case 53:  // Number 5                   // Sends morse code for "hi".
+                        enablePTT();
+                        sendMessage("hi");
+                        disablePTT();
+                        break;
+                    case 54:  // Number 6                   // Sends morse code with call sign. 
+                        enablePTT();
+                        sendMessage(idString);
+                        disablePTT();
+                        break;
+                    case 55:  // Number 7                   // Sends morse code saying "73".
+                        enablePTT();
+                        sendMessage("73");
+                        disablePTT();
+                        break;
+                    default:  // Any other number           // Turn off transmissions.
+                        beaconEnabled = false;  
+                        break;
+                }
+            }
+
+            break;
+
+
+        case State::BEACON:
+            // If beaconEnabled is false then bail out of this state and go back to WAIT.
+            if (!beaconEnabled) {
+                state = State::WAIT;
+                break;
+            }
+
+            // Send the beacon.
+            enablePTT();
+            playMelody();
+            delay(1000);  
+            sendMessage(beaconString);
+            delay(500);
+            disablePTT();
+
+            // Restart the timer for the next beacon cycle.
+            timerBeacon.reset();
+            break;
+
+
+        default:
+            // Strange state condition. Make everything safe.
+            state = State::WAIT;
+            beaconEnabled = false;
+            break;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////
+//                     FUNCTIONS                       //
+/////////////////////////////////////////////////////////
+
+/**
+ * @brief Sends the fox hunt melody.
+ */
+void playMelody() {
+    // Melody related.
+    int melody[] = { NOTE_G4, NOTE_E4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_G4, NOTE_G4, NOTE_E4 };
+    int noteDurations[] = { 8, 8, 4, 4, 8, 8, 8, 8, 4, 4, 4, 4 }; // 4 = quarter note, 8 = eighth note, etc.
+    int noteSize = sizeof(melody)/sizeof(*melody);
+
+    for (int thisNote = 0; thisNote < noteSize; thisNote++) {
+        int noteDuration = 1000 / noteDurations[thisNote];
+        tone(PIN_BUZZER, melody[thisNote], noteDuration);
+
+        // To distinguish the notes, set a minimum time between them. The note's duration + 30% seems to work well.
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+
+        // stop the tone playing:
+        noTone(PIN_BUZZER);
+    }
+}
+
+
+
+/**
+ * @brief Sends an alphanumeric message in Morse Code.
+ * @param msg The alphanumeric char string to send.
+ */
+void sendMessage(const char* msg) {
+    for (int i = 0; i < sizeof(msg); i++){
+        sendCharacter(msg[i]);
+    }
+}
+
+
+
+/**
+ * @brief Converts a single alphanumeric character to Morse Code dot(s) and dash(es) and sends them.
+ * @param c The alphanumeric character to send.
+ */
+void sendCharacter(const char c) {
+    for (int i = 0; i < alphabet.length(); i = i + 1) {
+        if (alphabet[i] == c || ALPHABET[i] == c) {
+            sendMorseCode(morseCode[i]);
+            return;
         }
-        digitalWrite(PTT, HIGH);
-        delay(2000);
-        break;
-      case 55:  //number 7                // Sends morse code saying "73"
-        digitalWrite(PTT, LOW);
-        delay(2000);
-        sendLetter('7');
-        sendLetter('3');
-        digitalWrite(PTT, HIGH);
-        delay(2000);
-        break;
-      default:  // any other number, turn off transmissions
-        xmit = 0;  // set the flag to disable transmissions
-      break;
     }
-  }
-  delay(1);
+    //    if (c == '\n')
+    //       Serial.println();
 }
 
-if (xmit == 1)
-  {
-  digitalWrite(PTT, LOW);
-  delay(2000); // delay 2 seconds after PTT to account for race condition
 
-// play a little melody
-  for (int thisNote = 0; thisNote < 12; thisNote++) {
-    // to calculate the note duration, take one second
-    // divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000 / noteDurations[thisNote];
-    tone(BUZZER_PIN, melody[thisNote], noteDuration);
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    // stop the tone playing:
-    noTone(BUZZER_PIN);
-  }
 
-  delay(1000);  //one second between melody and beaconstring
-
-  for (int i = 0; i < sizeof(beaconstring); i++){
-    sendLetter(beaconstring[i]);
-    }
-
-  delay(500);
-  digitalWrite(PTT, HIGH);  // drop the PTT and wait a while
-//  randNumber = random(10000, 20000);  // remove comments and add comments to delay(5000)
-//  Serial.print(randNumber);           // to use random delays
-//  Serial.println();
-//  delay(randNumber);
-//  delay(5000);
-  }
-}
-
-// End of Loop()
-// Functions follow
-
-void sendLetter (char c) {
-   int i;
-   for (i = 0; i < alphabet.length(); i = i + 1) {
-       if (alphabet[i] == c || ALPHABET[i] == c) {
-           //Serial.print(c);
-           sendMorseCode(morseCode[i]);
-           return;
-       }
-   }
-   if (c == '\n')
-      Serial.println();
-}
-
-void sendMorseCode (String tokens) {
-   int i;
-   for (i = 0; i < tokens.length(); i = i + 1) {
+/**
+ * @brief Takes a sequence of Morse Code dot(s) and dash(es) and sends them as individual dot(s) and dash(es).
+ * @param tokens A string of Morse Code "tokens". This is a sequence of dot(s) and dash(es) which represent a Morse Code letter.
+ */
+void sendMorseCode(String tokens) {
+   for (int i = 0; i < tokens.length(); i = i + 1) {
        switch (tokens[i]) {
            case '-':
                sendDash();
@@ -218,43 +291,81 @@ void sendMorseCode (String tokens) {
                break;
        }
    }
-   morseOutputOff(2);
-//   Serial.print(" ");
+
+    // Time to wait between each symbol.
+    morseToneWait(2);
 }
 
+
+
+/**
+ * @brief Sends a Morse Code dot, of the proper frequency, to the appropriate pin on the controller.
+ */
 void sendEndOfWord() {
-   morseOutputOff(4);
-//   Serial.print("  ");
+    // Send a silent pause to signify the end of a word.
+    noTone(PIN_BUZZER);
+    morseToneWait(4);
 }
 
-//basic functions - Morse code concepts  
+
+
+/**
+ * @brief Sends a Morse Code dot, of the proper frequency, to the appropriate pin on the controller.
+ */
 void sendDot() {
-   tone(BUZZER_PIN, BUZZER_FREQUENCY);
-   morseOutputOn(1);
-   noTone(BUZZER_PIN);
-   morseOutputOff(1);
-//   Serial.print(".");
+    // Send the dot.
+    tone(PIN_BUZZER, CW_FREQ);
+    morseToneWait(1);
+
+    // Send the silent pause after the dot.
+    noTone(PIN_BUZZER);
+    morseToneWait(1);
 }
+
+
+
+/**
+ * @brief Sends a Morse Code dash, of the proper frequency, to the appropriate pin on the controller.
+ */
 void sendDash() {
-   tone(BUZZER_PIN, BUZZER_FREQUENCY);
-   morseOutputOn(3);
-   noTone(BUZZER_PIN);
-   morseOutputOff(1);
-//   Serial.print("-");
+    // Send the dash.
+    tone(PIN_BUZZER, CW_FREQ);
+    morseToneWait(3);
+
+    // Send the silent pause after the dash.
+    noTone(PIN_BUZZER);
+    morseToneWait(1);
 }
 
-//Low level functions - how the actions are accomplished
-// n = number of intervals 
-// interval is a fixed length of time determined at start, for example 200 milliseconds
-void morseOutputOn (int n) {
-//   example: morseOutputOn(1) means turn output on and keep it on for 1 interval 
-//            morseOutputOn(3) means turn output on and keep it on for 3 intervals 
-//   
-//   digitalWrite(morseOutput, HIGH);
-   delay(n * interval);
+
+
+/**
+ * @brief Delays code execution for a period of time equal to the duration of a Morse Code dot times the n argument passed in.
+ *        The reason for this to be called is that the tone function, that sends the CW signal, is non-blocking, but we
+ *        actually need for the code to wait on the transmission of each CW tone prior to continuing. Thus we need to 
+ *        force a deley of the proper amount of time.
+ * @param n Number of dot durations to delay for.
+ */
+void morseToneWait(const int n) {
+    delay(n * dotDuration);
 }
 
-void morseOutputOff (int n) {
-//   digitalWrite(morseOutput, LOW);
-   delay(n * interval);
+
+
+/**
+ * @brief Enables the Push-to-talk pin/relay.
+ */
+void enablePTT() {
+    digitalWrite(PIN_PTT, LOW);
+    delay(2000);
+}
+
+
+
+/**
+ * @brief Disables the Push-to-talk pin/relay.
+ */
+void disablePTT() {
+    digitalWrite(PIN_PTT, HIGH);
+    delay(2000);
 }
